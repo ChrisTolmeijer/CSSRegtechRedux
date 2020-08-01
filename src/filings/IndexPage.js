04626ab01cf5c4aa8235b27ab7f5def9122e1d2a
Chris Tolmeijer <chris@crizit.com>
2020-08-01 16:33:38 -0400

    Add 'add filing' button

--- src/filings/IndexPage.js
+++ src/filings/IndexPage.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { FilingsList } from './List';
 import { Statistics } from './Statistics';
 import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 export const IndexPage = () => {
 
+    const history = useHistory();
     const [ filings, setFilings ] = useState([]);
 
     const fetchFilings = () => {
@@ -20,5 +22,9 @@ export const IndexPage = () => {
     return <div className="page-width">
         <Statistics filings={filings}></Statistics>
         <FilingsList filings={filings}></FilingsList>
+        <hr />
+        <button onClick={() => {
+            history.push('/add');
+        }}>Add filing</button>
     </div>;
 };
